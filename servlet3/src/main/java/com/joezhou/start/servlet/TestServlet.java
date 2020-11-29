953943d064a4bb25afac1fd1259632601c24e0f1
joezhou <yy06200210@163.com>
2020-11-29 15:01:30 +0800

    servlet即时页面

---
+++ servlet3/src/main/java/com/joezhou/start/servlet/TestServlet.java
@@ -0,0 +1,41 @@
+package com.joezhou.start.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * @author JoeZhou
+ */
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // 因为请求头中允许接收html类型[Accept:text/html...]
+        // 所以我可以放心的设置内容的MIME类型为text/html
+        resp.setContentType("text/html;charset=utf-8");
+
+        // 利用IO流从服务端向页面端进行传输[响应]
+        PrintWriter writer = resp.getWriter();
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<meta charset=\"UTF-8\">");
+        writer.println("<title>servlet-test</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("系统时间:" + new Date());
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
