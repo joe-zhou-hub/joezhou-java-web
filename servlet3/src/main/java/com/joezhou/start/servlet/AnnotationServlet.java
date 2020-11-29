c33c3baff23f2d837fc1692f1d129778cfc16e7f
joezhou <yy06200210@163.com>
2020-11-29 16:33:19 +0800

    servlet注解配置

---
+++ servlet3/src/main/java/com/joezhou/start/servlet/AnnotationServlet.java
@@ -0,0 +1,42 @@
+package com.joezhou.start.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * @author JoeZhou
+ */
+@WebServlet({"/servlet/annotation_a", "/servlet/annotation_b"})
+public class AnnotationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Request Headers [Accept:text/html...]
+        resp.setContentType("text/html;charset=utf-8");
+
+        // response data
+        PrintWriter writer = resp.getWriter();
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<meta charset=\"UTF-8\">");
+        writer.println("<title>xml-servlet</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("系统时间:" + new Date());
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
