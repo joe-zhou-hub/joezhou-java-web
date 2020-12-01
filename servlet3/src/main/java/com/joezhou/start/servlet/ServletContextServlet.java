0d31f9c10620460278a8487a3a937d71d88b7419
joezhou <yy06200210@163.com>
2020-12-01 15:21:19 +0800

    servlet全局配置

---
+++ servlet3/src/main/java/com/joezhou/start/servlet/ServletContextServlet.java
@@ -0,0 +1,25 @@
+package com.joezhou.start.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author JoeZhou
+ */
+@WebServlet("/api/servlet/servlet_context")
+public class ServletContextServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(super.getServletContext().getInitParameter("loc"));
+        System.out.println(super.getServletContext().getInitParameter("type"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
