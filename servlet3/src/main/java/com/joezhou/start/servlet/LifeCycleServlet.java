23003558831cd51411310a30e3d482cd351c0c11
joezhou <yy06200210@163.com>
2020-12-01 15:52:46 +0800

    servlet生命周期

---
+++ servlet3/src/main/java/com/joezhou/start/servlet/LifeCycleServlet.java
@@ -0,0 +1,34 @@
+package com.joezhou.start.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author JoeZhou
+ */
+@WebServlet(value = "/api/servlet/life_cycle", loadOnStartup = 1)
+public class LifeCycleServlet extends HttpServlet {
+    @Override
+    public void destroy() {
+        System.out.println("destroy()...");
+    }
+
+    @Override
+    public void init() {
+        System.out.println("init()...");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println("doGet()...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
