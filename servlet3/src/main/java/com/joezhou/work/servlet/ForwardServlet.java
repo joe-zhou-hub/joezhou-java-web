880af1ecaa2c417e56e28a2d06e99f827540ea17
joezhou <yy06200210@163.com>
2020-12-05 10:16:58 +0800

    请求转发

---
+++ servlet3/src/main/java/com/joezhou/work/servlet/ForwardServlet.java
@@ -0,0 +1,26 @@
+package com.joezhou.work.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author JoeZhou
+ */
+@WebServlet("/api/forward")
+public class ForwardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        System.out.println("转发给/view/html/login.html");
+        req.getRequestDispatcher("/view/html/login.html").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
