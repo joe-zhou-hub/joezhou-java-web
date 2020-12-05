7e2f54e8352b28ead2d00fba90c2b988598adda6
joezhou <yy06200210@163.com>
2020-12-05 11:06:17 +0800

    请求转发

---
+++ servlet3/src/main/java/com/joezhou/work/servlet/RedirectServlet.java
@@ -0,0 +1,26 @@
+package com.joezhou.work.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author JoeZhou
+ */
+@WebServlet("/api/redirect")
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding("UTF-8");
+        System.out.println("重定向到/view/html/login.html");
+        resp.sendRedirect(req.getContextPath() + "/view/html/login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
