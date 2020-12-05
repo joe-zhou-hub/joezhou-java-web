5f2d6ce4036e4e2d50c33fff3f7a4ef9f6ffa90d
joezhou <yy06200210@163.com>
2020-12-05 09:35:11 +0800

    三大存值域线程安全问题

---
+++ servlet3/src/main/java/com/joezhou/work/servlet/ResponseServlet.java
@@ -0,0 +1,32 @@
+package com.joezhou.work.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author JoeZhou
+ */
+@WebServlet("/api/response")
+public class ResponseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie cookie = new Cookie("cookie-name", "cookie-value");
+        cookie.setMaxAge(60 * 5);
+        resp.addCookie(cookie);
+        resp.addHeader("string-header", "string-header");
+        resp.addIntHeader("int-header", 9527);
+        resp.addDateHeader("date-header", System.currentTimeMillis());
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().print("response-data");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
