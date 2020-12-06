be6d80cb04fbb151aafd4ba360076dfc231dea9b
joezhou <yy06200210@163.com>
2020-12-06 09:21:47 +0800

    JACKSON

--- servlet3/src/main/java/com/joezhou/work/pojo/Account.java
+++ servlet3/src/main/java/com/joezhou/work/pojo/Account.java
@@ -1,6 +1,8 @@
 package com.joezhou.work.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -8,6 +10,8 @@ import java.io.Serializable;
  * @author JoeZhou
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account implements Serializable {
     private Integer id;
     private String username;
--- servlet3/src/main/java/com/joezhou/work/servlet/JsonServlet.java
+++ servlet3/src/main/java/com/joezhou/work/servlet/JsonServlet.java
@@ -3,6 +3,7 @@ package com.joezhou.work.servlet;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.joezhou.work.pojo.Account;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,9 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author JoeZhou
@@ -36,20 +35,39 @@ public class JsonServlet extends HttpServlet {
     }
 
     private void jackson(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        Map<String, Object> map = new HashMap<>(2);
-        map.put("map-name", "map-val-1");
-        map.put("map-null", null);
-        map.put("map-empty", "");
-        map.put("map-date", new Date());
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy/MM/dd hh:mm:ss"));
 
-        String jsonStr = objectMapper.writeValueAsString(map);
-        resp.setContentType("application/json;charset=utf-8");
-        resp.getWriter().print(jsonStr);
+        Map<String, Object> map = new HashMap<>(2);
+        map.put("map-name", "map-val-1");
+        map.put("map-null", null);
+        map.put("map-empty", "");
+        map.put("map-date", new Date());
+        System.out.println(objectMapper.writeValueAsString(map));
+
+        Account account = new Account(9527,"admin","123");
+        System.out.println(objectMapper.writeValueAsString(account));
+
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new Account(9528,"zhaosi","zhaosi123"));
+        accounts.add(new Account(9529,"liuneng","liuneng123"));
+        accounts.add(new Account(9530,"dajiao","dajiao123"));
+        System.out.println(objectMapper.writeValueAsString(accounts));
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map01 = new HashMap<>(2);
+        map.put("map01-key01", "map01-value01");
+        map.put("map01-key02", "map01-value02");
+        Map<String, Object> map02 = new HashMap<>(2);
+        map.put("map02-key01", "map02-value01");
+        map.put("map02-key02", "map02-value02");
+        list.add(map01);
+        list.add(map02);
+        resp.setContentType("text/html;charset=utf-8");
+        objectMapper.writeValue(resp.getWriter(), list);
     }
 
     @Override
