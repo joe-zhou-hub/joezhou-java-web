f02b3d8e19d65f3070e0d26a72d130f4e6e216e8
joezhou <yy06200210@163.com>
2020-12-09 10:07:15 +0800

    GSON

---
+++ z-note/servlet3/z-res/JsonUtil.java
@@ -0,0 +1,54 @@
+package com.joezhou.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author JoeZhou
+ */
+public class JsonUtil {
+
+    /**
+     * build string in JSON format for front
+     *
+     * @param statusCode the status code of response
+     * @param message    the status message of response
+     * @param data       the data of response
+     * @return string in JSON format
+     */
+    public static String build(int statusCode, String message, Object data) {
+        String result = "";
+
+        Map<String, Object> map = new HashMap<>(3);
+        map.put("statusCode", statusCode);
+        map.put("message", message);
+        map.put("data", data);
+
+        ObjectMapper objectMapper = new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setDateFormat(new SimpleDateFormat("yyyy/MM/dd hh:mm:ss"));
+
+        try {
+            result = objectMapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            System.out.println("writeValueAsString() error...");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String build(Object data) {
+        return build(200, "success", data);
+    }
+
+    public static String build(int statusCode, String message) {
+        return build(200, "success", null);
+    }
+}
---
+++ z-note/servlet3/z-res/PageUtil.java
@@ -0,0 +1,114 @@
+package com.joezhou.util;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author JoeZhou
+ */
+@Data
+public class PageUtil implements Serializable {
+
+    private Integer size, page, pages, total;
+    private List<String> numbersList;
+
+    /**
+     * Specify the current page number and the size per page
+     *
+     * @param page the current page number, default 1, String for controller...
+     * @param size the size per page, default 5, String for controller...
+     */
+    public PageUtil(String page, String size) {
+        try {
+            int realPage = Integer.parseInt(page);
+            int realSize = Integer.parseInt(size);
+            if (realPage > 0 && realSize > 0) {
+                this.page = realPage;
+                this.size = realSize;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PageUtil() {
+        this("1", "5");
+    }
+
+    /**
+     * build suffix of mysql's paging SQL statment
+     *
+     * @return suffix of mysql's paging SQL statment
+     */
+    public String getLimitSuffix() {
+        return " LIMIT " + ((this.page - 1) * size) + ", " + this.size;
+    }
+
+    /**
+     * set number of total entries and calculate pages
+     *
+     * @param total number of total entries
+     */
+    public void buildTotalAndPages(int total) {
+        this.total = total;
+        double size = this.size;
+        this.pages = (int) Math.ceil(total / size);
+    }
+
+    /**
+     * build a paging list of numbers
+     */
+    public void buildNumbersList() {
+
+        numbersList = new ArrayList<>();
+
+        // when pages >= 2, ["1"], ["2"] is requried
+        int constLength = 2;
+
+        // uncertain numbers in page list must <= 5
+        int varLength = 5;
+
+        // numbers in page list must <= 7
+        int maxLength = 7;
+
+        // ["1"] must exist
+        numbersList.add("1");
+
+        if (pages > 1) {
+
+            // when pages > 1, ["2"] must exist
+            numbersList.add("2");
+
+            if (page <= varLength) {
+
+                // add number after ["2"], <=pages and <=5
+                for (int i = constLength; i < maxLength && i < pages; i++) {
+                    numbersList.add(i + 1 + "");
+                }
+
+                // when pages > 7, add ["..."]
+                if (pages > maxLength) {
+                    numbersList.add("...");
+                }
+            } else {
+                // when page > 5, the first ["..."] must exist
+                numbersList.add("...");
+
+                // add two number before current page
+                // add two number after current page
+                // <= pages
+                for (int i = -constLength; i <= constLength && page + i <= pages; i++) {
+                    numbersList.add(page + i + "");
+                }
+
+                // add second ["..."] when pages > 7 and at least two more
+                if (pages > maxLength && page < pages - constLength) {
+                    numbersList.add("...");
+                }
+            }
+        }
+    }
+}
